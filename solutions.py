21d54f80e292f7b677f801ff48a2718bc51dd55c
Kaizhe Zhang <kaizhezhang458@gmail.com>
2026-06-01 17:17:42 -0400

    complete boolean expressions practice

--- solutions.py
+++ solutions.py
@@ -13,7 +13,8 @@ def is_sweltering():
 
       :returns: True if the temperature is over 90, False otherwise.
     """
-    # write your code for this function below this line.
+    temp = int(input("What is the current temperature in Fahrenheit? "))
+    return temp > 90
 
 
 def is_warm():
@@ -24,7 +25,8 @@ def is_warm():
 
       :returns: True if the temperature is between 75 and 87, inclusive, False otherwise.
     """
-    # write your code for this function below this line.
+    temp = int(input("What is the current temperature in Fahrenheit? "))
+    return temp >= 75 and temp <= 87
 
 
 def is_humid():
@@ -35,7 +37,8 @@ def is_humid():
 
       :returns: True if it is humid today, False otherwise.
     """
-    # write your code for this function below this line.
+    answer = input("Is it humid today? ")
+    return answer == "yes"
 
 
 def is_inclement():
@@ -46,7 +49,8 @@ def is_inclement():
 
       :returns: True if it is raining, snowing, or sleeting today, False otherwise.
     """
-    # write your code for this function below this line.
+    forecast = input("What is the weather forecast today? ")
+    return forecast == "rain" or forecast == "snow" or forecast == "sleet"
 
 
 def is_typical_new_york_summer():
@@ -59,7 +63,7 @@ def is_typical_new_york_summer():
 
       :returns: True if the temperature is over 90 and it is humid, False otherwise.
     """
-    # write your code for this function below this line.
+    return is_sweltering() and is_humid()
 
 
 def is_cool_and_nice():
@@ -71,4 +75,4 @@ def is_cool_and_nice():
 
       :returns: True if the weather is cool and nice today, False otherwise.
     """
-    # write your code for this function below this line.
+    return not is_sweltering() and not is_warm() and not is_humid() and not is_inclement()
